13674f99c2ca477d127491b28feb7d00bfd01c09
FyslAhmd <info.faysal.32@gmail.com>
2026-06-08 11:07:39 +0600

    fix: Correct spelling of "favorites" to "favourites" in response messages

--- src/app/modules/favorite/favorite.controller.ts
+++ src/app/modules/favorite/favorite.controller.ts
@@ -12,7 +12,7 @@ const toggleFavorite: RequestHandler = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: 200,
     success: true,
-    message: result.status === "added" ? "Added to favorites" : "Removed from favorites",
+    message: result.status === "added" ? "Added to favourites" : "Removed from favourites",
     data: result,
   });
 });
@@ -24,7 +24,7 @@ const getMyFavorites: RequestHandler = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: 200,
     success: true,
-    message: "Favorites retrieved successfully",
+    message: "Favourites retrieved successfully",
     data: result,
   });
 });
@@ -36,7 +36,7 @@ const getMyFavoriteIds: RequestHandler = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: 200,
     success: true,
-    message: "Favorite IDs retrieved successfully",
+    message: "Favourite IDs retrieved successfully",
     data: result,
   });
 });
